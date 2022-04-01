aec3eca43cd41efb13794a3d68d332dac7d536fb
Sumitsha11 <97402908+Sumitsha11@users.noreply.github.com>
2022-04-01 20:10:12 +0530

    lst dc

--- extra/ab.py
+++ extra/ab.py
@@ -1,11 +1,34 @@
+g="{"
+m="}"
+for i in range(10):
+    a=f"""            
+            var countDownDate{i} = new Date("April 6, 2022 19:30:00").getTime();
+            
 
-a="""                        <div class="row row-cols-2 mt-2">
-                            <div class="col-3">
-                                <input type="checkbox" name="B1" id="B" value="Evin Lewis" style="height: 50px; width: 50px;" class="bat" onclick="b1()">
-                            </div>
-                            <div class="col">
-                                <p style="font-size: 25px;">Evin Lewis</p>
-                            </div>
-                        </div>"""
-a.replace("","\n")
-print(a)
+            var x{i} = setInterval(function() {g}
+            
+
+              var now{i} = new Date().getTime();
+            
+              // Find the distance between now and the count down date
+              var distance{i} = countDownDate{i} - now{i};
+            
+              // Time calculations for days, hours, minutes and seconds
+              var days{i} = Math.floor(distance{i} / (1000 * 60 * 60 * 24));
+              var hours{i} = Math.floor((distance{i} % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+              var minutes{i} = Math.floor((distance{i} % (1000 * 60 * 60)) / (1000 * 60));
+              var seconds{i} = Math.floor((distance{i} % (1000 * 60)) / 1000);
+            
+              // Display the result in the element with id="demo"
+              document.getElementById("MK").innerHTML = days{i} + "d " + hours{i} + "h "
+              + minutes{i} + "m " + seconds{i} + "s ";
+            
+              // If the count down is finished, write some text
+              if (distance{i} < 0) {g}
+                clearInterval(x);
+                document.getElementById("MK").innerHTML = "STARTED";
+                document.getElementById("link_MK").remove()
+
+              {m}
+            {m}, 1000);"""
+    print(a)
